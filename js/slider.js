89e0844cda1a012c8862a512343baf2f76dd255a
Copeland Peacock <peacockc@wit.edu>
2022-03-21 22:32:24 -0400

    done for the night

--- js/slider.js
+++ js/slider.js
@@ -2,3 +2,14 @@ var slider = document.getElementById("SpeechRate");
 var sliderOutPut = document.getElementById("value");
 
 sliderOutPut.innerHTML = slider.value;
+
+slider.oninput = function() {
+    sliderOutPut.innerHTML = this.value;
+}
+
+slider.addEventListener("mousemove", function() {
+    var xAxis = slider.value;
+    var color = 'linear-gradient(90deg, #f26135 ' + xAxis +'%, #000000 ' + xAxis + '%)';
+    slider.style.background = color;
+
+});
