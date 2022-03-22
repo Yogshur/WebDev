224cfc0755f4b502fdd209d70529c3a3f78ce04a
Copeland Peacock <peacockc@wit.edu>
2022-03-22 13:14:52 -0400

    Almost done with the slider bars

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,41 @@
-var slider = document.getElementById("SpeechRate");
-var sliderOutPut = document.getElementById("value");
+//Speech SLider
+var sliderSpeech = document.getElementById("SpeechRate");
+var sliderSpeechOutPut = document.getElementById("valueSpeech");
 
-sliderOutPut.innerHTML = slider.value;
+sliderSpeechOutPut.innerHTML = sliderSpeech.value + ' x';
 
-slider.oninput = function() {
-    sliderOutPut.innerHTML = this.value;
+sliderSpeech.oninput = function() {
+    sliderSpeechOutPut.innerHTML = this.value + 'x';
+}
+sliderSpeech.addEventListener("mousemove", function() {
+    var xAxis = sliderSpeech.value;
+    if(xAxis == .5) {
+        xAxis = 25;
+    } else if (xAxis == 1) {
+        xAxis = 50;
+    } else if (xAxis == 1.5) {
+        xAxis = 75;
+    } else if (xAxis == 2) {
+        xAxis = 100;
+    }
+    var color = 'linear-gradient(90deg, #f26135 ' + xAxis +'%, #000000 ' + xAxis + '%)';
+    sliderSpeech.style.background = color;
+
+});
+
+
+//Volume Slider
+var sliderVolume = document.getElementById("volume");
+var sliderVolumeOutPut = document.getElementById("valueVolume");
+
+sliderVolumeOutPut.innerHTML = sliderVolume.value;
+
+sliderVolume.oninput = function() {
+    sliderVolumeOutPut.innerHTML = this.value;
 }
 
-slider.addEventListener("mousemove", function() {
-    var xAxis = slider.value;
+sliderVolume.addEventListener("mouse", function() {
+    var xAxis = sliderVolume.value;
     var color = 'linear-gradient(90deg, #f26135 ' + xAxis +'%, #000000 ' + xAxis + '%)';
-    slider.style.background = color;
-
+    sliderVolume.style.background = color;
 });
